c1dc8af6d28410666b102eb07db47bcba3aad4a0
Mileide-Mikka <mi-leyde@hotmail.com>
2021-10-04 20:44:24 -0300

    Foram feitas as seguintes alteraçõe: formatação das telas para mobile, tablet e pc. Foi feita ligação entre as pags Register e Login, foi alterado o nome da form de form1 foi para forme-register.

--- validate.js
+++ validate.js
@@ -2,12 +2,12 @@ function ValidateEmail(inputText) {
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if (inputText.value.match(mailformat)) {
     alert("Valid email address!");
-    document.form1.text1.focus();
+    document.form-register.text1.focus();
     return true;
   }
   else {
     alert("You have entered an invalid email address!");
-    document.form1.text1.focus();
+    document.form-register.text1.focus();
     return false;
   }
 }

5b625699c3f5da369442b03348190f860c9cf655
Mileide-Mikka <mi-leyde@hotmail.com>
2021-10-07 21:41:41 -0300

    Foi introduzido a pag Home e nela o navbar no header, com link para Contact e Register

---
+++ dark-mode.js
@@ -0,0 +1,13 @@
+/*const $html = document.querySelector('html');
+const $btn = document.querySelector('.btn-mode');
+const $text = document.getElementById("default");
+const $icon = document.getElementById("icon");
+$btn.addEventListener('click',function(){
+  $html.classList.toggle('dark-mode');
+  $icon.classList.toggle("fa-sun");
+  if ($text.innerHTML == "Dark Mode") {
+        $text.textContent = "Light Mode";
+  } else {
+        $text.textContent = "Dark Mode";
+  }
+});*/
